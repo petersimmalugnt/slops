0a6a5c0fda9861c4ee57fc3a75c4873664872e2c
petersimmalugnt <peter@simmalugnt.se>
2024-04-11 12:38:13 +0200

    changed 4 files

--- webpack.config.js
+++ webpack.config.js
@@ -3,13 +3,14 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const TerserJSPlugin = require("terser-webpack-plugin");
 const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 
-const jsFileName = "main";
-const cssFileName = "style";
-
 module.exports = {
-  entry: "./src/js/main.js",
+  entry: {
+    main: "./src/js/main.js", // Huvudsaklig JavaScript-ingångspunkt
+    style: "./src/css/main.scss", // Huvudsaklig CSS-ingångspunkt genererad från SCSS
+  },
   output: {
-    filename: `${jsFileName}.js`,
+    // Dynamiskt namn baserat på entry-punktens namn
+    filename: "[name].js", // Använd '[name].js' här för att undvika konflikter
     path: path.resolve(__dirname, "dist"),
   },
   module: {
@@ -37,7 +38,8 @@ module.exports = {
   },
   plugins: [
     new MiniCssExtractPlugin({
-      filename: `${cssFileName}.css`,
+      // Säkerställ att CSS-filerna har ett unikt namn
+      filename: "[name].css", // Använd '[name].css' här
     }),
   ],
   optimization: {
